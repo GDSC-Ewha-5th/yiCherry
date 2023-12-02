e18b13e2ccc1c979cc21ba4b50c1741dd162546d
official-Cherry <reebon7@gmail.com>
2023-12-02 14:10:45 +0900

    getUser,getRepo error

---
+++ gptapi/src/components/ResultCard.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
+
+const ResultCard = ({ data, repoData }) => {
+
+  // destructuring
+  const { avatar_url, name, login, followers, following, bio } = data;
+  const { name:repoName } = repoData;
+
+  return (
+    <>
+      <StDiv>
+        <StImg src={avatar_url} alt={`${login}'s avatar`}/>
+        <h3>{name}</h3>
+        <ul>
+          <StList><StText>GitHub ID: {login}</StText></StList>
+          <StList><StText>Followers: {followers}</StText></StList>
+          <StList><StText>Following: {following}</StText></StList>
+          <StList><StText>Bio: {bio}</StText></StList>
+        </ul>
+      </StDiv>
+    </>
+  );
+};
+
+const StDiv = styled.div`
+  display: block;
+`;
+
+const StImg = styled.img`
+  width: 100;
+  height: 100;
+  border-radius: 70%;
+`;
+
+const StList = styled.li`
+  display: block;
+  margin-block: 5px;
+`;
+
+const StListItem = styled.li`
+  display: block;
+  margin-block: 5px;
+  list-style: none;
+`;
+
+const StText = styled.span`
+  display: inline-block;
+  padding: 10px;
+  border: 1px solid #dddddd;
+  border-radius: 24px;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export default ResultCard;
---
+++ gptapi/src/lib/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+/************* baseURL 사용 **************/ 
+const client = axios.create({
+  baseURL: "https://api.github.com/users/",
+});
+
+// Export userData
+export const getUserData = async (name) => { // name == githubID 
+  try {
+    const { data } = await client.get(name); 
+    console.log("[SUCCESS] GET user data", data);
+    return data;
+  } 
+  catch (e) {
+    console.log("[FAIL] GET user data", e);
+		return null;
+  }
+};
+
+// Export repoData - Advanced assignment
+export const getUserRepo = async (name) => { 
+  try {
+    const { data } = await client.get(`${name}/repos`);
+    console.log("[SUCCESS] GET user repositories", data?.slice(0,10));
+    return data
+  } 
+  catch (e) {
+    console.log("[FAIL] GET user repositories", e);
+    return null;
+  }
+};
